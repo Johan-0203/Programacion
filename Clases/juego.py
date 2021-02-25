92d738514f1cbb3ed9669f618b229b9d7c1417d8
Johan-0203 <chaconjohan2003@gmail.com>
2021-02-25 07:48:21 -0500

    juego

--- Clases/juego.py
+++ Clases/juego.py
@@ -8,12 +8,18 @@ pregunta_numero='''
         Ingresa tu numero: '''
 mensaje_perder='No manches we, ya la regaste, ese numero no era, vuelve a poner otro: '
 despedida='Felicidades, ganaste, hasta luego'
-
+mensaje_sin_vidas='Ya no te quedan mas vidas'
 #Entradas
 num_ocul=6
+life=3
 print(bienvenido)
 numero=int(input(pregunta_numero))
-while(numero!=num_ocul):
+if(numero!=num_ocul):
+    life=life-1
+while(numero!=num_ocul and life>0):
     numero=int(input(mensaje_perder))
-
-print(despedida)
+    life=life-1
+if(life==0):
+    print(mensaje_sin_vidas)
+else:
+    print(despedida)
