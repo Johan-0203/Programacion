3d2057185eec8b9072d749fdf65fe039e56c1176
Johan-0203 <chaconjohan2003@gmail.com>
2021-03-04 08:12:41 -0500

    Ok

--- Clases/juego2.py
+++ Clases/juego2.py
@@ -4,10 +4,14 @@ bienvenida='Bienvenido al juego con while'
 pregunta_numero='''
         En este juego tienes que ingresar
         un numero que vaya del 1 al 10,
-        la idea es que lo puedas intentar
-        con 3 vidas, no existe vida 0...
+        no existe vida 0...
         Ingresa tu numero: '''
-pregunta_fallida='No manches we, ya la regaste, ese numero no era, vuelve a poner otro: '
+pregunta_numero_2 = '''
+        Vuelve a ingresar un numero
+        del 1 al 10.
+        Ya veremos si sigues con la misma suerte
+        Ingresa tu numero: '''
+pregunta_fallida='Ese no era, vuelve a poner otro: '
 mensaje_ganar='Felicidades, ganaste'
 mensaje_sin_vidas='Ya no te quedan mas vidas'
 despedida='Nos vemos pronto'
@@ -16,6 +20,9 @@ pregunta_dificultad='''
         2- Moderado
         3- Dificil
 '''
+mensaje_segundo_nivel = 'Entonces, has escogido el camino a la muertee, bienvenido al segundo nivel'
+mensaje_caliente ='Caliente, bajale un poco'
+mensaje_frio='Frio frio, como agua del rio'
 #Entradas
 dificultad=int(input(pregunta_dificultad))
 vidas=None
@@ -24,25 +31,45 @@ while(dificultad != 1 and dificultad != 2 and dificultad != 3):
         dificultad=int(input(pregunta_dificultad))
 
 if(dificultad == 1):
-        vidas = 5
+        vidas = 10
         print('Dificultad facil seleccionada')
 elif(dificultad == 2):
-        vidas = 3
+        vidas = 5
         print('Dificultad normal seleccionada')
 else:
-        vidas =1
+        vidas =2
         print('Modo admin activado')
 
 
 num_oculto=random.randint(1,10)
+num_oculto_2 = random.randint(1,10)
 num_ingresado=int(input(pregunta_numero))
 while(num_ingresado!=num_oculto and vidas>1):
-    vidas=vidas-1
-    print ('Te quedan', vidas, 'vidas')
-    num_ingresado = int(input(pregunta_fallida))
+        if(num_ingresado > num_oculto):
+                print(mensaje_caliente)
+        else:
+                print(mensaje_frio)
+        vidas=vidas-1
+        print ('Te quedan', vidas, 'vidas')
+        num_ingresado = int(input(pregunta_fallida))
 
-if(vidas >= 0 and num_ingresado == num_oculto):
+if(num_ingresado == num_oculto and vidas >= 0):
+        print('-'*30)
+        print(mensaje_segundo_nivel)
+        num_ingresado=int(input(pregunta_numero_2))
+        while(num_ingresado!=num_oculto_2 and vidas>1):
+                if(num_ingresado > num_oculto_2):
+                        print(mensaje_caliente)
+                else:
+                        print(mensaje_frio)
+                vidas=vidas-1
+                print ('Te quedan', vidas, 'vidas')
+                num_ingresado=int(input(pregunta_fallida))
+
+
+if(vidas >= 0 and num_ingresado == num_oculto_2):
         print(mensaje_ganar)
 else:
         print(mensaje_sin_vidas)
-        print('El numero oculto era', num_oculto)
+        print('El numero oculto 1 era', num_oculto, 'y el numero oculto 2 era', num_oculto_2)
+print('-'*30)
