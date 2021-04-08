b3cd3321012cbe5a697c6a5762fd867ca283e602
Johan-0203 <chaconjohan2003@gmail.com>
2021-04-07 19:28:53 -0500

    ultima clase

--- Clases/Funciones/funciones.py
+++ Clases/Funciones/funciones.py
@@ -1,17 +1,18 @@
 #----------Sumar dos números -----------#
 def sumar (a = 0, b = 0):
     '''
-        devuelve la suma de dos numeros a y b
-        por defecto a y b valen 0
+        devuelve la suma de dos números a y b
+        por defecto a vale cero al igual que b
     '''
     suma = a + b
     return suma
+
+def linedesign(cantidad = 10, simbolo = '#'):
+    print(simbolo *cantidad)
+    return None
+
 #----------Restar dos números -----------#
 def restar (a = 0, b = 0):
-    '''
-        devuelve la resta de dos numeros a y b
-        por defecto a y b valen 0
-    '''
     resta = a - b
     return resta
 #----------Multiplicar dos números -----------#
@@ -22,12 +23,18 @@ def multiplicar (a = 0, b = 0):
 def dividir (a = 0, b = 1):
     dividi = a / b
     return dividi
-
-#-------potenciar dos numeros----------
-def potenciar(base = 0, exponente=1):
-    potencia=base**exponente
+#----------potenciar dos números -----------#
+def potenciar (base = 0, exponente = 1):
+    potencia = base ** exponente
     return potencia
+#----------funciones dependientes de otras -----------#
+def calcular (operacion, numeroA, numeroB):
+    print(operacion(numeroA,numeroB))
 
-#-------funciones depenientes de otras-------
-def calcular (operacion, numA, numB):
-    print(operacion(numA,numB))
+def mostrarLista(lista):
+    for elemento in lista:
+        print(elemento)
+
+def mostrar2Lista(lista1, lista2):
+    for i in range(len(lista1)):
+        print(lista1[i],'\t',lista2[i])
