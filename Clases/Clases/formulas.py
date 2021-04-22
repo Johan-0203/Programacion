3d7e7da2d48c9c3f426508e0f221ef080aaad8ab
Johan-0203 <chaconjohan2003@gmail.com>
2021-04-22 00:09:33 -0500

    clases y objetos act

---
+++ Clases/Clases/formulas.py
@@ -0,0 +1,17 @@
+print('hola')
+x = int (input('ingrese el valor'))
+y = int (input('ingrese el valor'))
+w = int (input('ingrese el valor'))
+
+for i in range(x):
+    for k in range(y):
+        for z in range(w):
+            print(z)
+
+for i in range(x):
+    print(i)
+
+for j in range(w):
+    print(j)
+
+print('chaoo')
