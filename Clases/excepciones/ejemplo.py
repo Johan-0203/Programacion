49abc5629a2df3d2729710a5e95110a9539948cc
Johan-0203 <chaconjohan2003@gmail.com>
2021-05-18 07:17:53 -0500

    excepciones

--- Clases/excepciones/ejemplo.py
+++ Clases/excepciones/ejemplo.py
@@ -4,11 +4,46 @@ while(isCorrectInfo == False):
         edad = int(input('Ingrese su edad: '))
         isCorrectInfo=True
     except ValueError:
-        print('Ingresaste un mensaje no valido')
+        print('Ingresaste un dato no valido')
 
 nombreArchivo = input('Ingrese el archivo que desee encontrar: ')
 try:
     archivo = open(nombreArchivo)
 except FileNotFoundError:
     print(f'El archivo {nombreArchivo} no se ha encontrado')
+base = 4
+divisor = 0
+try:
+    dividir = base / divisor
+except ZeroDivisionError:
+    print('El divisor ingresado es 0 por lo tanto es imposible dividir')
 
+nombre = 'Daniel'
+print(nombre.isalpha())
+assert(nombre.isalpha())
+
+isCorrectInfo=False
+while(isCorrectInfo == False):
+    try:
+        nombre = input('Ingrese su nombre: ')
+        assert(nombre.isalpha())
+        isCorrectInfo=True
+    except AssertionError:
+        print('Ingresaste un dato no valido')
+
+isCorrectInfo=False
+while(isCorrectInfo == False):
+    try:
+        edad = int(input('Ingrese su edad: '))
+        assert(edad >= 18)
+        isCorrectInfo=True
+    except AssertionError:
+        print('Los menores de edad no peuden entrar')
+    except ValueError:
+        print('Edades son numeros enteros')
+
+lista=[2,8,7,6]
+try:
+    lista[5]
+except IndexError:
+    print('Indice es mayor al tamaño de la lista')
